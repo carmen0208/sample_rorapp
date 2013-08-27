1a63a88e16a56ee3337229ffedf6fe25b50de4d8
carmen0208 <13770576@qq.com>
2013-08-27 17:35:08 +0800

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 SampleApp::Application.routes.draw do
-  get "users/new"
+  
+  #get "users/new"
+  resources :users
   #get "static_pages/home"
   root to: 'static_pages#home'
   match '/signup',  to: 'users#new',            via: 'get'
---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name     "Carmen Liu"
+    email    "13770576@qq.com"
+    password "foobar"
+    password_confirmation "foobar"
+  end
+end
